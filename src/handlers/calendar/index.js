5f3e4fd6bee89edcad36807543f4d0eef94fdc30
riktothepast <rik@502studios.net>
2018-11-23 23:02:26 -0600

    displays upcoming events, allows to set the number of results (5 by default)

--- src/handlers/calendar/index.js
+++ src/handlers/calendar/index.js
@@ -5,14 +5,30 @@ const eventList = require('./eventList');
 let calendarList;
 let listEvents;
 
+function generateParams(words) {
+  const params = {
+    maxResults: 5,
+    timeMin: (new Date()).toISOString(),
+    singleEvents: true,
+    orderBy: 'startTime',
+  }
+  words.forEach(element => {
+    if(!isNaN(element)) {
+      params.maxResults = element;
+    }
+  });
+
+  return params;
+}
+
 function configure(config) {
   gCalendar(config.googleCalendar.privateKey, config.googleCalendar.scopes).then(cEvents => {
     calendarList = cEvents.list;
     listEvents = eventList(calendarList, config.googleCalendar.primary);
   });
 
-  function handler(msg) {
-    listEvents(10)
+  function handler(msg, keywords) {
+    listEvents(generateParams(keywords))
       .then(message => msg.reply(message))
       .catch(message => msg.reply(message));
   }
